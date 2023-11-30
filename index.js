c923bfe0008240c8c185fd026f7bba9e4ec8da89
Benny Wick <benny.wick@gmail.com>
2023-11-30 12:14:02 -0800

    update to use re-usable methods

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 let homeScoreEl = document.getElementById('home-score')
 let guestScoreEl = document.getElementById('guest-score')
 
-function addHomeOne() {
-    let result = parseInt(homeScoreEl.textContent)
-    homeScoreEl.textContent = result + 1
+function addOne(btnEl) {
+    console.log(btnEl)
+    if (btnEl.id === 'add-home') {
+        let result = parseInt(homeScoreEl.textContent)
+        homeScoreEl.textContent = result + 1
+    } else {
+        let result = parseInt(guestScoreEl.textContent)
+        guestScoreEl.textContent = result + 1
+    }
 }
 
-function addHomeTwo() {
-    let result = parseInt(homeScoreEl.textContent)
-    homeScoreEl.textContent = result + 2
+function addTwo(btnEl) {
+    if (btnEl.id === 'add-home') {
+        let result = parseInt(homeScoreEl.textContent)
+        homeScoreEl.textContent = result + 2
+    } else {
+        let result = parseInt(guestScoreEl.textContent)
+        guestScoreEl.textContent = result + 2
+    }
 }
 
-function addHomeThree() {
-    let result = parseInt(homeScoreEl.textContent)
-    homeScoreEl.textContent = result + 3
+function addThree(btnEl) {
+    if (btnEl.id === 'add-home') {
+        let result = parseInt(homeScoreEl.textContent)
+        homeScoreEl.textContent = result + 3
+    } else {
+        let result = parseInt(guestScoreEl.textContent)
+        guestScoreEl.textContent = result + 3
+    }
 }
 
-function resetHome() {
-    homeScoreEl.textContent = 0
-}
-
-function addGuestOne() {
-    let result = parseInt(guestScoreEl.textContent)
-    guestScoreEl.textContent = result + 1
-}
-
-function addGuestTwo() {
-    let result = parseInt(guestScoreEl.textContent)
-    guestScoreEl.textContent = result + 2
-}
-
-function addGuestThree() {
-    let result = parseInt(guestScoreEl.textContent)
-    guestScoreEl.textContent = result + 3
-}
-
-function resetGuest() {
-    guestScoreEl.textContent = 0
-
+function resetScore(btnEl) {
+    if (btnEl.id === 'add-home') {
+        homeScoreEl.textContent = 0
+    } else {
+        guestScoreEl.textContent = 0
+    }
 }
